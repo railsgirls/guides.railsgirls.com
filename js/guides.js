375a257415cf372f061bd4e5913b0e21b54fe8c1
Vesa Vänskä <vesan2000@gmail.com>
2013-04-13 23:11:43 +0300

    Adds saving of OS selection to a cookie

--- js/guides.js
+++ js/guides.js
@@ -5,11 +5,22 @@ $(document).ready(function() {
 
   $(".os-specific").append("<span class='picker'>Choose your operating system: <a href='#' class='win-link'>Windows</a> <a href='#' class='nix-link'>Other</a></span>").find(".win-link").click(function(event) {
     event.preventDefault();
+    $.cookie("os", "win", { expires: 1825, path: '/' }); // expires in 5 years
+
     $(".os-specific .win-link").addClass("active").siblings().removeClass("active");
     $(".os-specific").children("div").hide().filter(".win").show();
   }).end().find(".nix-link").click(function(event) {
     event.preventDefault();
+    $.cookie("os", "nix", { expires: 1825, path: '/' }); // expires in 5 years
+
     $(".os-specific .nix-link").addClass("active").siblings().removeClass("active");
     $(".os-specific").children("div").hide().filter(".nix").show();
-  }).end().find(".win-link").click();
+  })
+
+  var osFromCookie = $.cookie("os");
+  if(osFromCookie) {
+    $(".os-specific").find("." + osFromCookie + "-link").click();
+  } else {
+    $(".os-specific").find(".win-link").click();
+  }
 });
